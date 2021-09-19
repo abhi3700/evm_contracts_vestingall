1cc3ec671aaf488ea37cb32d77b51c4b80b8dca1
Abhijit Roy <dalvath3700@gmail.com>
2021-09-19 20:12:55 +0530

    vesting contract modified

---
+++ contracts/interfaces/IToken.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.2;
+
+
+interface IToken {
+  // function balanceOf(address owner) external view returns (unit);
+  // function allowance(address owner, address spender) external view returns (unit);
+  // function approve(address spender, uint value) external returns (bool);
+  // function transfer(address to, uint value) external returns (bool);
+  // function transferFrom(address from, address to, uint value) external returns (bool);
+  function transferByVestingC(address recipient, uint256 amount) public returns (bool);
+}
